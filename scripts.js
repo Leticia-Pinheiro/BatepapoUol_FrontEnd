10c9cbb92763809fae52f27fd0ce5177175158c9
Letícia Pinheiro <leticia.gomezpinheiro@gmail.com>
2022-04-16 20:57:29 -0300

    layout 1

---
+++ scripts.js
@@ -0,0 +1,14 @@
+// const promisseEntrar = axios.get("https://mock-api.driven.com.br/api/v6/uol/participants ")
+// const promisseMConexao = axios.get("https://mock-api.driven.com.br/api/v6/uol/status")
+// const promisseBuscarMsg = axios.get("https://mock-api.driven.com.br/api/v6/uol/messages")
+// const promisseEnviarMsg = axios.get("https://mock-api.driven.com.br/api/v6/uol/messages")
+
+let nome = prompt ("Informe seu nome:")
+
+// se nome nao estiver no servidor, criar um usuario, entrar na sala 
+// se estiver, pedir outro nome 
+
+// promisseEntrar.then(function (name){
+    
+
+// })
