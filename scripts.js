be6bb35462e89d213861c8922145371165a67cb3
Letícia Pinheiro <leticia.gomezpinheiro@gmail.com>
2022-04-17 13:08:47 -0300

    Informar nome

--- scripts.js
+++ scripts.js
@@ -2,13 +2,48 @@
 // const promisseMConexao = axios.get("https://mock-api.driven.com.br/api/v6/uol/status")
 // const promisseBuscarMsg = axios.get("https://mock-api.driven.com.br/api/v6/uol/messages")
 // const promisseEnviarMsg = axios.get("https://mock-api.driven.com.br/api/v6/uol/messages")
+let nomes = []
+let mensagens = []
+let nome = prompt("Qual o seu nome?")
 
-let nome = prompt ("Informe seu nome:")
+pegarNomes();
 
-// se nome nao estiver no servidor, criar um usuario, entrar na sala 
-// se estiver, pedir outro nome 
+function pegarNomes(){
+    const promisse = axios.get("https://mock-api.driven.com.br/api/v6/uol/participants")
+    promisse.then(carregarNomes)    
+}
 
-// promisseEntrar.then(function (name){
-    
+function carregarNomes(response){
+    nomes = response.data
+    adicionarNome()
+}
 
-// })
+function adicionarNome(){      
+    const novoNome = {
+        name: nome
+    }
+    const promisse = axios.post("https://mock-api.driven.com.br/api/v6/uol/participants", novoNome)
+
+    promisse.then(ok)
+    promisse.catch(corrigirErro)
+}
+
+function ok(){
+    alert("ok")
+    // pegarMensagens()
+}
+
+function corrigirErro(error){    
+    if(error.response.status === 400)    
+        nome = prompt("Já existe usuário com esse nome. Informe outro");      
+        pegarNomes()
+}
+
+
+
+
+
+
+
+
+
